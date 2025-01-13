d0735b48c74b2d968d57bb9b5a6031013073846d
Nandan R Pai <nandanrp09@gmail.com>
2025-01-13 21:02:14 +0530

    refac: zues bust component

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,35 +1,21 @@
-import { Canvas } from "@react-three/fiber";
 import { ZeusBackground } from "./zeusBackground";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { useLoader } from "@react-three/fiber";
-import React, { Suspense, useEffect, useState } from "react";
-import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { caesarDressing } from "~/pages/_app";
 import DustOverlay from "./dustOverlay";
-import { Float } from "@react-three/drei";
 import GlowingHackfest from "./glowingHackfest";
+import dynamic from "next/dynamic";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Float } from "@react-three/drei";
+// import ZeusBust from "./zeusBust";
+
+const ZeusBust = dynamic(() => import("./zeusBust"), { ssr: false });
 
 const Hero: React.FC = () => {
   return (
     <main className="relative mx-auto flex w-full items-center justify-center overflow-hidden bg-[#2b2a2a]">
       <ZeusBackground />
       <DustOverlay />
-
-      <div className="absolute z-30 h-full w-full">
-        <Canvas className="flex  items-center justify-center">
-          <ambientLight intensity={0.5} />
-          <directionalLight
-            position={[0, 1, 5]}
-            intensity={2}
-            color={"#E0E0E0"}
-          />
-          <Suspense fallback={null}>
-            <Float rotationIntensity={2}>
-              <Model />
-            </Float>
-          </Suspense>
-        </Canvas>
-      </div>
+      <ZeusBust />
 
       <div className="absolute bottom-9 z-50 flex flex-col ">
         <GlowingHackfest />
@@ -47,53 +33,6 @@ const Hero: React.FC = () => {
         </div>
       </div>
     </main>
-  );
-};
-
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, "/zeusHF.glb", (loader) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath(
-      "https://www.gstatic.com/draco/versioned/decoders/1.5.7/",
-    ); // Set the path to the Draco decoder
-    loader.setDRACOLoader(dracoLoader);
-  });
-  const [scale, setScale] = useState([2.5, 2.5, 2.5]);
-  const [rotation, setRotation] = useState([0, 0, 0]);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setScale([1.5, 1.5, 1.5]);
-      } else {
-        setScale([2.5, 2.5, 2.5]);
-      }
-    };
-
-    const handleMouseMove = (event: MouseEvent) => {
-      const { clientX, clientY } = event;
-      const xRotation = (clientY / window.innerHeight - 0.5) * 0.2;
-      const yRotation = (clientX / window.innerWidth - 0.5) * 0.2;
-      setRotation([xRotation, 0 + yRotation * 10, 0]);
-    };
-
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("mousemove", handleMouseMove);
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
-  return (
-    <primitive
-      object={gltf.scene}
-      scale={scale}
-      position={[0, 0, 0]}
-      rotation={rotation}
-    />
   );
 };
 
