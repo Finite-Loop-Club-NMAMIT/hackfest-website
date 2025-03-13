9d24ad23b68d12ce7fc8863eb5fce4fe4d14b9a0
shishirkarkeraa <dn.shishirgk@gmail.com>
2025-03-13 17:28:07 +0530

    fix: procedure

--- src/server/api/routers/analytics.ts
+++ src/server/api/routers/analytics.ts
@@ -1,4 +1,4 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { adminProcedure, createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 import { db } from "~/server/db";
 export const analyticsRouter = createTRPCRouter({
