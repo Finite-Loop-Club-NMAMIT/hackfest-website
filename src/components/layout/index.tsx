b1cad016b080c3563014a107d40066cf2c55366b
len-mendonca <lenmendonca2003@gmail.com>
2025-03-18 17:26:43 +0530

    feat: marquee

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,13 +3,15 @@ import Navbar from "../navbar";
 import Footer from "../footer";
 import ProgressBarProvider from "../progressBarProvider";
 import { useRouter } from "next/router";
+import Marquee from "../marquee";
 export default function RootLayout(props: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
     <div
-      className={`dark text-white ${router.pathname === "/" ? "" : "bg-black"}`}
+      className={`dark relative text-white ${router.pathname === "/" ? "" : "bg-black"}`}
     >
+      <Marquee />
       <ProgressBarProvider>
         <Toaster richColors expand={false} position="bottom-center" />
         <Navbar />
